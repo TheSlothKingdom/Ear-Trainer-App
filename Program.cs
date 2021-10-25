18a8f459917715d1e4b80e8ef4081464324b3172
Joel Murtaugh <84286506+TheSlothKingdom@users.noreply.github.com>
2021-10-25 19:43:29 -0400

    added audio player

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NetCoreAudio;
 
 namespace Ear_Trainer_App
 {
@@ -9,11 +10,8 @@ namespace Ear_Trainer_App
             bool shoudILoop = true;
             while (shoudILoop) 
             {
-
-
                 Console.WriteLine("Hello, where would you like to go?\nPress 'Q' and 'Enter' for quiz\nPress 'L' and 'Enter' for library\nPress 'E' and 'Enter' to exit");
                 string navigationStart = Console.ReadLine().ToLower();
-
 
                 if (navigationStart == "q")
                 {
@@ -21,7 +19,8 @@ namespace Ear_Trainer_App
                 }
                 else if (navigationStart == "l")
                 {
-
+                    
+       
                 }
                 else if (navigationStart == "e")
                 {
